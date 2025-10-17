0739c4f61a4302e4e6e75d987b691eebfd22f4a2
poojarivinod <vinod07081@gmail.com>
2025-10-17 20:18:29 +0530

    blue

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,10 @@ app.use(cors());
 
 //Health Checking
 app.get('/health',(req,res)=>{
-    //res.json("This is the health check");
+    res.json("This is the health check");
     //res.json("This is the health check for blue-1.0.0");
     //res.json("This is the health check for green-2.0.0");
-    res.json("This is the health check for blue-3.0.0");
+    //res.json("This is the health check for blue-3.0.0");
 });
 
 // ADD TRANSACTION
